8793b48bdcc5035506f11190aa845feb1770b7aa
Kazuki3dayo <fate0007rx7@yahoo.co.jp>
2020-10-21 22:20:50 +0900

    既読機能のサーバーサイド実装　コメント付き

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -9,14 +9,14 @@ class PostsController < ApplicationController
   end
 
   def checked
-    post = Post.find(params[:id])
+    post = Post.find(params[:id]) #先ほど設定したURLパラメーターから、既読したメモのidが渡されるように設定するので、そのidを使用して該当するレコードを取得しています。
     if post.checked 
-      post.update(checked: false)
-    else
-      post.update(checked: true)
+      post.update(checked: false) #if文で、post.checkedという既読であるか否かを判定するプロパティを指定し、
+    else                          #既読であれば「既読を解除するためにfalseへ変更」し、既読でなければ「既読にするためtrueへ変更」します。
+      post.update(checked: true)  #この時はupdateというActiveRecordのメソッドを使用して更新しています。
     end
 
-    item = Post.find(params[:id])
-    render json: { post: item }
+    item = Post.find(params[:id]) #最後に、更新したレコードをitem = Post.find(params[:id])で取得し直し、
+    render json: { post: item }   #render json:{ post: item }でJSON形式（データ）としてchecked.jsに返却しています。
   end
 end
